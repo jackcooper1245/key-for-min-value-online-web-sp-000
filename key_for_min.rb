419c0102989c16aea1d8d6fd6af767dd65cd2ae9
Jack Cooper <jackcooper1245@gmail.com>
2020-04-09 15:58:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,9 @@ def key_for_min_value(name_hash)
   #binding.pry
   value = nil
   key = nil
-name_hash.each do |name, hash|
+name_hash.each do |name, number|
   binding.pry
+if number > value
+  value = number
 end
 end
