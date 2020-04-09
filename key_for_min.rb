4502bf5d443948ae7d0d6ef2fecd0fa7315685a1
Jack Cooper <jackcooper1245@gmail.com>
2020-04-09 15:48:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   new_array = []
 name_hash.each do |name, hash|
-  if hash[0] < hash[1] && hash[0] < hash[2]
+  if hash < hash[1] && hash[0] < hash[2]
     new_array << name_hash[0]
     elsif hash[1] < hash[0] && hash[1] < hash[2]
     new_array << name_hash[1]
