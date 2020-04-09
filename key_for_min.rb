efa343907e2c4fb62c82cf144faea7b62b88d506
Jack Cooper <jackcooper1245@gmail.com>
2020-04-09 15:37:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-min_value = 0
-
+name_hash.each do |name, hash|
+  if hash[0] < hash[1] && hash[0] < hash[2]
+    return name_hash[0]
+    elsif hash[1] < hash[0] && hash[1] < hash[2]
+    return name_hash[1]
+  else 
+    return name_hash[2]
+  end
+end
 end
